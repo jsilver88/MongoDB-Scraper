41404a232e815f49867821ae209a288eb6da6d5f
Jared Silver <jrsilver88@gmail.com>
2020-06-14 08:57:27 -0700

    created get routes for home and saved views and get route for scraping from the website

--- server.js
+++ server.js
@@ -13,18 +13,12 @@ let db = require("./models");
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-const router = express.Router();
-// Require routes file
-require("./routes/routes")(router);
-
 // Initialize Express
 let app = express();
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 // Make public a static folder
 app.use(express.static(__dirname + "/public"));
-
-app.use(router);
 
 // Connect to Mongo DB
 mongoose.Promise = Promise;
@@ -35,6 +29,60 @@ mongoose.connect(MONGODB_URI, function (err) {
         console.log("mongoose connection is successful.");
     }
 });
+
+// Get articles from the db
+app.get("/", function (req, res) {
+    db.Article.find({
+        saved: false
+    },
+        function (err, dbArticle) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("home", {
+                    articles: dbArticle
+                });
+            }
+        })
+})
+
+// Get route for scraping the website
+app.get("/scrape", function (req, res) {
+    axios.get("https://techcrunch.com/", function (response) {
+        let $ = cheerio.load(response.data);
+        $("div.post-block").each(function (i, element) {
+            let title = $(element).find("a.post-block__title__link").text().trim();
+            let url = $(element).find("a.post-block__title__link").attr("href");
+            let description = $(element).children(".post-block__content").text().trim();
+
+            if (title && url && description) {
+                db.Article.create({
+                    title: title,
+                    url: url,
+                    description: description
+                }).then(function (dbArticle) {
+                    console.log(dbArticle);
+                }).catch(function (err) {
+                    console.log(err);
+                })
+            }
+        })
+    })
+});
+
+// Get route for saved articles
+app.get("/saved", function (req, res) {
+    db.Article.find({
+        saved: true
+    }).then(function (dbArticle) {
+        res.render("saved", {
+            articles: dbArticle
+        })
+    }).catch(function (err) {
+        res.json(err);
+    })
+});
+
 
 // Connect Handlebars to express
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
